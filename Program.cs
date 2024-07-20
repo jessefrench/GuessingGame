5679403780ef3e262f29829eb52f8d877f4f81dd
Jesse <frejes32@gmail.com>
2024-07-20 13:13:11 -0500

    Completed phases 7 and 8

--- Program.cs
+++ Program.cs
@@ -1,38 +1,97 @@
-﻿Random random = new Random();
+﻿Console.WriteLine(@"Welcome to The Guessing Game!
+Please choose a difficulty level:
+1. Easy
+2. Medium
+3. Hard
+4. Cheater");
+
+string difficulty = Console.ReadLine();
+int attempts = 0;
+bool infiniteAttempts = false;
+Random random = new Random();
 int secretNumber = random.Next(1, 101);
 
-Console.WriteLine("Psst! Hey you! Can you guess the secret number? I'll give you 4 chances ;)");
-
-for (int i = 0; i < 4; i++)
+switch (difficulty)
 {
-    Console.Write($"Attempt {i + 1}: ");
-    int.TryParse(Console.ReadLine(), out int playerGuess);
-
-    if (playerGuess == secretNumber)
-    {
-        Console.WriteLine($"Wow, you guessed {playerGuess}. That's correct... How did you do that? Are you cheating?");
+    case "1":
+        attempts = 8;
         break;
-    }
-    else
+    case "2":
+        attempts = 6;
+        break;
+    case "3":
+        attempts = 4;
+        break;
+    case "4":
+        infiniteAttempts = true;
+        break;
+    default:
+        Console.WriteLine("Invalid difficulty level. Defaulting to Hard.");
+        attempts = 4;
+        break;
+}
+
+if (infiniteAttempts)
+{
+    Console.WriteLine("Can you guess the secret number? You can keep guessing until you get it right.");
+
+    while (true)
     {
-        int guessesLeft = 3 - i;
+        int.TryParse(Console.ReadLine(), out int playerGuess);
 
-        if (playerGuess > secretNumber)
+        if (playerGuess == secretNumber)
         {
-            Console.WriteLine($"Ha! You guessed {playerGuess}. That's too high!");
+            Console.WriteLine($"Wow, you guessed {playerGuess}. That's correct... How did you do that? Are you cheating?");
+            break;
         }
         else
         {
-            Console.WriteLine($"Ha! You guessed {playerGuess}. That's too low!");
+            if (playerGuess > secretNumber)
+            {
+                Console.WriteLine($"Ha! You guessed {playerGuess}. That's too high!");
+            }
+            else
+            {
+                Console.WriteLine($"Ha! You guessed {playerGuess}. That's too low!");
+            }
         }
+    }
+}
+else
+{
+    Console.WriteLine($"Can you guess the secret number? You get {attempts} attempts.");
 
-        if (guessesLeft > 0)
+    for (int i = 0; i < attempts; i++)
+    {
+        Console.Write($"Attempt {i + 1}: ");
+        int.TryParse(Console.ReadLine(), out int playerGuess);
+
+        if (playerGuess == secretNumber)
         {
-            Console.WriteLine($"You have {guessesLeft} guesses left.");
+            Console.WriteLine($"Wow, you guessed {playerGuess}. That's correct... How did you do that? Are you cheating?");
+            break;
         }
         else
         {
-            Console.WriteLine("You've used all your chances. Bye now!");
+            int guessesLeft = attempts - i - 1;
+
+            if (playerGuess > secretNumber)
+            {
+                Console.WriteLine($"Ha! You guessed {playerGuess}. That's too high!");
+            }
+            else
+            {
+                Console.WriteLine($"Ha! You guessed {playerGuess}. That's too low!");
+            }
+
+            if (guessesLeft > 0)
+            {
+                Console.WriteLine($"You have {guessesLeft} guesses left.");
+            }
+            else
+            {
+                Console.WriteLine("You've used all your chances. Bye now!");
+            }
         }
     }
 }
