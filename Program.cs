edb1ad35c75cffa1061a712f79b2825889ebfc3f
Jesse <frejes32@gmail.com>
2024-07-20 11:39:52 -0500

    Completed phase 3

--- Program.cs
+++ Program.cs
@@ -1,13 +1,19 @@
 ﻿int secretNumber = 42;
 
-Console.WriteLine("Can you guess the secret number?");
-int.TryParse(Console.ReadLine(), out int playerGuess);
+Console.WriteLine("Can you guess the secret number? You have 4 attempts.");
 
-if (playerGuess == secretNumber)
+for (int i = 0; i < 4; i++)
 {
-    Console.WriteLine("Wow, you actually got it right. How did you do that? Are you cheating?");
-}
-else
-{ 
-    Console.WriteLine("Ha! Not even close. Bye now!");
+    Console.Write($"Attempt {i + 1}: ");
+    int.TryParse(Console.ReadLine(), out int playerGuess);
+
+    if (playerGuess == secretNumber)
+    {
+        Console.WriteLine("Wow, you actually got it right. How did you do that? Are you cheating?");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Ha! Not even close.");
+    }
 }
