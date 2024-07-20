5716aeaa5ca942d14db25be775df52db21e793dd
Jesse <frejes32@gmail.com>
2024-07-20 11:12:47 -0500

    Completed phase 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Can you guess the secret number?");
+int.TryParse(Console.ReadLine(), out int guess);
+Console.WriteLine($"You guessed: {guess}");
